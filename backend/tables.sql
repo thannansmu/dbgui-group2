4716ec9eba5f36acddfdab9609a1de884fb98aac
thannansmu <thannan@smu.edu>
2023-03-28 20:07:06 -0500

    Filled out some sql tables, need to add other tables and fix data types

--- backend/tables.sql
+++ backend/tables.sql
@@ -1,17 +1,50 @@
-CREATE Table Students(firstName CHAR(20), lastName CHAR(20), comments CHAR(20), password CHAR(20),
-studentID INTEGER, bio CHAR(20), username CHAR(20)
+-- Need to figure out data types.
+CREATE Table Students(
+    username CHAR(20), 
+    firstName CHAR(20), 
+    lastName CHAR(20), 
+    comments CHAR(20), 
+    password CHAR(20),
+    studentID INTEGER, 
+    bio CHAR(500), 
+    favoriteTutors CHAR(20), 
+    requests CHAR(20), 
+    rating CHAR(20), 
+    reviews CHAR(20)
+);
 
-)
+CREATE Table Tutors (
+    username CHAR(20), 
+    firstName CHAR(20), 
+    lastName CHAR(20),  
+    password CHAR(20),
+    tutorID INTEGER, 
+    bio CHAR(500), 
+    favoriteTutors CHAR(20), 
+    requests CHAR(20), 
+    rating CHAR(20), 
+    reviews CHAR(20),
+    timesAvaliable CHAR(20),
+    tutoringSessions CHAR(20),
+    subjectsTaught CHAR(20)
+);
 
 
-CREATE Table Tutors
+CREATE Table Administration (
+    username CHAR(20),
+    password CHAR(20),
+    adminID CHAR(20),
+    name CHAR(20),
+    bio CHAR(500),
+    reports CHAR(20),
+);
 
 
-CREATE Table Administration
+
+CREATE Table Question ();
 
 
-
-
+CREATE Table Report ();
 
 
 
