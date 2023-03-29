03652c2aeff05168ba9b9632029d8307b04b6442
thannansmu <thannan@smu.edu>
2023-03-28 19:03:16 -0500

    Started sql tables

---
+++ backend/tables.sql
@@ -0,0 +1,30 @@
+CREATE Table Students(firstName CHAR(20), lastName CHAR(20), comments CHAR(20), password CHAR(20),
+studentID INTEGER, bio CHAR(20), username CHAR(20)
+
+)
+
+
+CREATE Table Tutors
+
+
+CREATE Table Administration
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
