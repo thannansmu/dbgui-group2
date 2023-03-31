5d93c91d356fad4ad30568b2ed463d9d03cdd803
thannansmu <thannan@smu.edu>
2023-03-31 15:42:49 -0500

    Made outline for SQL tables and filled out first four

--- backend/tables.sql
+++ backend/tables.sql
@@ -1,63 +1,69 @@
--- Need to figure out data types.
 CREATE Table Students(
-    username CHAR(20), 
-    firstName CHAR(20), 
-    lastName CHAR(20), 
-    comments CHAR(20), 
-    password CHAR(20),
+    username VARCHAR(20), 
+    firstName VARCHAR(20), 
+    lastName VARCHAR(20), 
+    comments VARCHAR(500), 
+    password VARCHAR(20),
     studentID INTEGER, 
-    bio CHAR(500), 
-    favoriteTutors CHAR(20), 
-    requests CHAR(20), 
-    rating CHAR(20), 
-    reviews CHAR(20)
+    bio VARCHAR(500), 
+    rating INTEGER,
+    PRIMARY KEY (username, studentID)
 );
 
 CREATE Table Tutors (
-    username CHAR(20), 
-    firstName CHAR(20), 
-    lastName CHAR(20),  
-    password CHAR(20),
+    username VARCHAR(20), 
+    firstName VARCHAR(20), 
+    lastName VARCHAR(20),  
+    password VARCHAR(20),
     tutorID INTEGER, 
-    bio CHAR(500), 
-    favoriteTutors CHAR(20), 
-    requests CHAR(20), 
-    rating CHAR(20), 
-    reviews CHAR(20),
-    timesAvaliable CHAR(20),
-    tutoringSessions CHAR(20),
-    subjectsTaught CHAR(20)
+    bio VARCHAR(500), 
+    rating VARCHAR(20),
+    PRIMARY KEY (username, tutorID)
 );
 
 
 CREATE Table Administration (
-    username CHAR(20),
-    password CHAR(20),
-    adminID CHAR(20),
-    name CHAR(20),
-    bio CHAR(500),
-    reports CHAR(20),
+    username VARCHAR(20),
+    password VARCHAR(20),
+    adminID INTEGER,
+    firstName VARCHAR(20),
+    lastName VARCHAR(20),
+    bio VARCHAR(500),
+    PRIMARY KEY (username)
 );
 
-
-
-CREATE Table Question ();
+CREATE Table Question (
+    questionID INTEGER,
+    studentID INTEGER,
+    tutorID INTEGER,
+    questionText VARCHAR(500),
+    answer VARCHAR(500),
+    PRIMARY KEY (questionID, studentID, tutorID),
+    FOREIGN KEY (studentID) REFERENCES Students,
+    FOREIGN KEY (tutorID) REFERENCES Tutors,
+);
 
 
 CREATE Table Report ();
 
 
+CREATE Table FavoriteTutors ();
 
 
+CREATE Table Comments ();
 
 
+CREATE Table Reviews ();
 
 
+CREATE Table Requests ();
 
 
+CREATE Table TimesAvaliable ();
 
 
+CREATE Table TutoringSessions ();
 
 
-
+CREATE Table SubjectsTaught ();
 
