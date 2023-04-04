731af9450c02a3e1ab06325ab4d659ef87da48f4
lawrence-l20 <73035400+lawrence-l20@users.noreply.github.com>
2023-04-04 13:59:10 -0500

    minor updates

--- frontend/src/components/pages/Services.js
+++ frontend/src/components/pages/Services.js
@@ -1,4 +1,21 @@
 import React from 'react'
 import '../../App.css'
 
-export default function Services(){return <h1 className="services">services</h1>}
+export default function Services() {
+    return (
+      <div className="services">
+        <div className="services-text">
+        <h1 style={{position: 'absolute', top: 0, left: 0, color: 'black'}}>
+          Services
+        </h1>
+  
+          <p className="services-paragraph">
+            The Learning Liaison is a tutoring forum that offers a wide range of services to help students achieve their academic goals. Whether you need help with math, science, or English, our team of experienced tutors is here to help you succeed. Our one-on-one tutoring sessions are tailored to meet your individual needs, and we use the latest teaching techniques and tools to ensure that you get the most out of each session. We also offer group tutoring sessions, online tutoring, and homework help to make learning as convenient and accessible as possible. With our personalized approach to education and commitment to excellence, we are confident that we can help you achieve your academic dreams.
+          </p>
+  
+        </div>
+      </div>
+    );
+  }
+  
+  
