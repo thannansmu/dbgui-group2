78e08ac3ddc5a58eaf541344e39612e3f9c75a29
agenato <agenato@smu.edu>
2023-04-04 19:25:16 -0500

    Added comments in LoginForm

--- frontend/src/components/pages/LoginForm.jsx
+++ frontend/src/components/pages/LoginForm.jsx
@@ -5,6 +5,11 @@ export const LoginForm = ({ accountType }) => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
+    /*if accountType = student, tutor, or admin, call the backend
+      to see if the username & password exist in those tables*/
+
+    /*remember to got to call post route*/
+
     return <>
         <h1>Login</h1>
 
