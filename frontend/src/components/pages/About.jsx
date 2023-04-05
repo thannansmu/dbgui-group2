5d4cf24a566a6fa93f6afd46047b58b59c95b29c
agenato <agenato@smu.edu>
2023-04-04 19:29:57 -0500

    fixed compile errors caused by export

--- frontend/src/components/pages/About.jsx
+++ frontend/src/components/pages/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../../App.css'
 
-export default function About() {
+export const About = () => {
     return (
       <div className="About Page">
         
--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -2,11 +2,9 @@ import React from 'react';
 import '../../App.css';
 import HeroSection from '../HeroSection';
 
-function Home(){
-return(
-<>
-<HeroSection/>
-</>)
+export const Home = () => {
+    return (
+        <>
+            <HeroSection />
+        </>)
 }
-
-export default Home;
