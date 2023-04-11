f1aefdecbba0ddc43a51f71bc033d03ca010195e
thannansmu <thannan@smu.edu>
2023-04-11 14:24:57 -0500

    Made outline for routes we have so far

--- backend/server.js
+++ backend/server.js
@@ -25,62 +25,128 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.put('/parse', (req, res) => {
-    console.log(req.body)
-    
-    try {
-        const { first, last, age, admin } = req.body
-        const name = `${first} ${last}`
-        const isAdmin = admin ? "is an admin" : "is not an admin"
-
-        res.status(200)
-        res.send(`${name} is ${age} years old and ${isAdmin}`)
-    } catch (err) {
-        console.log(err)
-    }
-})
-
-app.get('/db', (req, res) => {
-    connection.query('SHOW TABLES', (err, rows, fields) =>{
-        if (err) throw err
-
-        console.log(rows)
-        res.status(200)
-        res.send(rows)
-    })
-})
-
-app.post('/user', (req, res) => {
-    const { first, last, age, admin } = req.body
-    const query = `INSERT INTO users (first_name, last_name, age, admin) VALUES ('${first}', '${last}', ${age}, ${admin})`
-    connection.query(query, (err, rows, fields) => {
-        if (err) throw err
-
-        console.log(rows)
-        res.status(200)
-        res.send("Successfully added user!")
-    })
-})
-
-app.get('/users', (req, res) => {
-    connection.query(`SELECT * FROM users`, (err, rows, fields) => {
-        if (err) throw err
-
-        res.status(200)
-        res.send(rows)
-    })
-})
-
-app.put('/users/clear', (req, res) => {
-    connection.query(`DELETE FROM users`, (err, rows, fields) => {
-        if (err) throw err
-
-        res.status(200)
-        res.send("Successfully cleared users!")
-    })
-})
-
 // Start server
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+app.get('/users', (req, res) => {
+    
+})
+
+app.get(`/users/${username}`, (req, res, username) => {
+    
+})
+
+app.get(`/users/${username}/${attribute}`, (req, res, username, attribute) => {
+    
+})
+
+app.post('/users/add', (req, res) => {
+    
+})
+
+app.put(`/users/${username}/${attribute}/update`, (req, res) => {
+    
+})
+
+app.get(`/users/${username}/requests`, (req, res) => {
+    
+})
+
+app.get(`/users/${username}/comments`, (req, res) => {
+    
+})
+
+app.post(`/users/${username}/add_request`, (req, res) => {
+    
+})
+
+app.post(`/users/${username}/add_comment`, (req, res) => {
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Old Stuff
+// app.put('/parse', (req, res) => {
+//     console.log(req.body)
+    
+//     try {
+//         const { first, last, age, admin } = req.body
+//         const name = `${first} ${last}`
+//         const isAdmin = admin ? "is an admin" : "is not an admin"
+
+//         res.status(200)
+//         res.send(`${name} is ${age} years old and ${isAdmin}`)
+//     } catch (err) {
+//         console.log(err)
+//     }
+// })
+
+// app.get('/db', (req, res) => {
+//     connection.query('SHOW TABLES', (err, rows, fields) =>{
+//         if (err) throw err
+
+//         console.log(rows)
+//         res.status(200)
+//         res.send(rows)
+//     })
+// })
+
+// app.post('/user', (req, res) => {
+//     const { first, last, age, admin } = req.body
+//     const query = `INSERT INTO users (first_name, last_name, age, admin) VALUES ('${first}', '${last}', ${age}, ${admin})`
+//     connection.query(query, (err, rows, fields) => {
+//         if (err) throw err
+
+//         console.log(rows)
+//         res.status(200)
+//         res.send("Successfully added user!")
+//     })
+// })
+
+// app.get('/users', (req, res) => {
+//     connection.query(`SELECT * FROM users`, (err, rows, fields) => {
+//         if (err) throw err
+
+//         res.status(200)
+//         res.send(rows)
+//     })
+// })
+
+// app.put('/users/clear', (req, res) => {
+//     connection.query(`DELETE FROM users`, (err, rows, fields) => {
+//         if (err) throw err
+
+//         res.status(200)
+//         res.send("Successfully cleared users!")
+//     })
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
