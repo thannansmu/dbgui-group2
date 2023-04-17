989214648a9068a0e1231a5f3541ba117059f5db
agenato <agenato@smu.edu>
2023-04-16 23:34:21 -0500

    Squashed commit of the following:
    commit 29397d552eb988d42084208e544c499a063bf15f
    Author: thannansmu <thannan@smu.edu>
    Date:   Sat Apr 15 16:44:27 2023 -0500
    
        Removed quotes
    
    commit 9965bc82d8f5fd43109df6076d1e22f43e64a025
    Author: thannansmu <thannan@smu.edu>
    Date:   Sat Apr 15 16:41:26 2023 -0500
    
        Added quotes for add_request and add_comment
    
    commit e06c4716d354053db537b4a7968637a4cb6793c5
    Author: Siddharth Bapat <txsiddharth@gmail.com>
    Date:   Sat Apr 15 16:36:19 2023 -0500
    
        Made changes to routes that adds requests, created route that adds comments
    
    commit b99ff03d6378660315ef9dc3637483d9882e5b96
    Author: thannansmu <thannan@smu.edu>
    Date:   Thu Apr 13 20:46:08 2023 -0500
    
        Fixed auto increment for ids and added code to add user to the table for their role
    
    commit c0da2461f15edd254dca397cd79ef6aee5153b60
    Author: thannansmu <thannan@smu.edu>
    Date:   Thu Apr 13 19:56:05 2023 -0500
    
        Added debug messages for DB routes
    
    commit 2bf88381078b127cd56f433e7a84bac768112e7a
    Author: thannansmu <thannan@smu.edu>
    Date:   Thu Apr 13 10:25:49 2023 -0500
    
        Fixed bugs, adding user, getting user, and deleting user works
    
    commit e8043e216ad2d67378f3d277759611473b3ec3d6
    Author: thannansmu <thannan@smu.edu>
    Date:   Wed Apr 12 22:13:02 2023 -0500
    
        Fixed minor typos
    
    commit 9effe6ea437e6025265321e506b5bdb8de1008b8
    Merge: a18fdf7 ad9e993
    Author: nabeelakhan17 <nabeela.w.khan@gmail.com>
    Date:   Wed Apr 12 21:21:22 2023 -0500
    
        Merge branch 'db-routes' of https://github.com/thannansmu/dbgui-group2 into db-routes
    
    commit a18fdf79bb9f25e6be7a03d6bdc64d7c591a49e3
    Author: nabeelakhan17 <nabeela.w.khan@gmail.com>
    Date:   Wed Apr 12 21:20:26 2023 -0500
    
        finishing the routes
    
    commit ad9e993f17ccba2d0f2676cab1fbb55fecd4cb9a
    Author: thannansmu <thannan@smu.edu>
    Date:   Wed Apr 12 20:43:15 2023 -0500
    
        Fixed issue in server.js
    
    commit 3ab4b27f8cd5776ec25f16d7d1a3d04c526b4cd4
    Author: thannansmu <thannan@smu.edu>
    Date:   Wed Apr 12 20:07:27 2023 -0500
    
        Added auto increment
    
    commit 93dd09f54f8b10cd2addf145c27f7b0544920f86
    Author: Siddharth Bapat <txsiddharth@gmail.com>
    Date:   Wed Apr 12 20:01:29 2023 -0500
    
        Added routes for deleting requests, comments

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'CoolPasswordThanks',
-  database: 'tutoringTables'
+  database: 'tutoringTables',
+  multipleStatements: true
 })
 
 connection.connect()
@@ -36,6 +37,7 @@ app.get('/users', (req, res) => {
     if (err) throw err
     res.status(200)
     res.send(rows)
+    console.log(rows)
   })
 })
 
@@ -45,6 +47,7 @@ app.get(`/users/:username`, (req, res) => {
     if (err) throw err
     res.status(200)
     res.send(rows)
+    console.log(rows);
   })
 })
 
@@ -55,19 +58,32 @@ app.get(`/users/:username/:attribute`, (req, res) => {
     if (err) throw err
     res.status(200)
     res.send(rows)
+    console.log(rows)
   })
-    
 })
 
 app.post('/users/add', (req, res) => {
     const {username, firstName, lastName, passWord, bio, userRole} = req.body
-    const query = `INSERT INTO User (username, firstName, lastName, passWord, bio, userRole) VALUES ('${username}', '${firstName}', '${lastName}', '${passWord}', '${bio}', '${userRole}')`
-    connection.query(query, (err, rows, fields) => {
+    const query = `INSERT INTO User (username, firstName, lastName, passWord, bio, userRole) VALUES ('${username}', '${firstName}', '${lastName}', '${passWord}', '${bio}', '${userRole}'); `;
+
+    let query2 = "";
+    if (userRole.toLowerCase() == "student") {
+      query2 += `INSERT INTO Students (username) VALUES ('${username}')`
+    }
+
+    else if (userRole.toLowerCase() == "tutor") {
+      query2 += `INSERT INTO Tutors (username) VALUES ('${username}')`
+    }
+
+    else if (userRole.toLowerCase() == "admin") {
+      query2 += `INSERT INTO Administration (username) VALUES ('${username}')`
+    }
+
+    connection.query(query + query2, (err, rows, fields) => {
       if (err) throw err
-      
-      console.log(rows)
       res.status(200)
       res.send("Successfully added user!")
+      console.log(`Inserted user into database\nusername = ${username}, firstName = ${firstName}, lastName = ${lastName}, password = ${passWord}, bio = ${bio}, userRole = ${userRole}`);
     })
 })
 
@@ -79,6 +95,7 @@ app.put(`/users/:username/:attribute/update`, (req, res) => {
       if (err) throw err
       res.status(200)
       res.send(`Updated ${attribute} for ${username}`)
+      console.log(`Updated ${attribute} for ${username} to ${updatedValue}`)
     })
 })
 
@@ -88,6 +105,7 @@ app.get(`/users/:username/requests`, (req, res) => {
     if (err) throw err
     res.status(200)
     res.send(rows)
+    console.log(rows)
   })
 })
 
@@ -97,24 +115,37 @@ app.get(`/users/:username/comments`, (req, res) => {
     if (err) throw err
     res.status(200)
     res.send(rows)
+    console.log(rows)
   })
 })
 
 app.post(`/users/:username/add_request`, (req, res) => {
+  const username = req.params.username;
   const {requestID, studentID, tutorID, request} = req.body
-  const query = `INSERT INTO Requests (requestID, studentID, tutorID, request) VALUES ('${requestID}', '${studentID}', '${tutorID}', '${request}')`
+  const query = `INSERT INTO Requests (requestID, username, studentID, tutorID, request) VALUES (${requestID},'${username}', ${studentID}, ${tutorID}, '${request}')`
   connection.query(query, (err, rows, fields) => {
     if (err) throw err
     
     console.log(rows)
     res.status(200)
     res.send("Successfully added Request!")
+    console.log(`Added request for ${username}\nrequestID = ${requestID} studentID = ${studentID} tutorID = ${tutorID} request = ${request}`)
   })
    
 })
 
 app.post(`/users/:username/add_comment`, (req, res) => {
+  const username = req.params.username;
+  const {commentID, commentRecieverID, comment} = req.body
+  const query = `INSERT INTO Comments (commentID, username, commentRecieverID, comment) VALUES (${commentID}, '${username}', ${commentRecieverID}, '${comment}')`
+  connection.query(query, (err, rows, fields) => {
+    if (err) throw err
     
+    console.log(rows)
+    res.status(200)
+    res.send("Successfully added Comment!")
+    console.log(`Added request for ${username}\ncommentID = ${commentID} commentRecieverID = ${commentRecieverID} comment = ${comment}`)
+  })
 })
 
 app.delete(`/users/:username/delete`, (req, res) => {
@@ -124,6 +155,7 @@ app.delete(`/users/:username/delete`, (req, res) => {
     if (err) throw err
     res.status(200)
     res.send(rows)
+    console.log(`Deleted user ${username} from database`)
   })
 })
 
@@ -133,6 +165,7 @@ app.delete(`/delete_request/:requestID`, (req, res) => {
     if (err) throw err
     res.status(200)
     res.send(rows)
+    console.log(`Deleted request with requestID ${requestID} from database`)
   })
   
 })
@@ -143,6 +176,7 @@ app.delete(`/delete_comment/:commentID`, (req, res) => {
     if (err) throw err
     res.status(200)
     res.send(rows)
+    console.log(`Deleted request with commentID ${commentID} from database`)
   })
   
 })
