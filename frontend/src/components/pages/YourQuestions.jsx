69e2d531a067abf85fd4bda9e129858b7d916cf5
lawrence-l20 <73035400+lawrence-l20@users.noreply.github.com>
2023-04-19 16:25:32 -0500

    More Profile Information
    Display More Profile information

--- frontend/src/components/pages/YourQuestions.jsx
+++ frontend/src/components/pages/YourQuestions.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-export const YourQuestions = () => {
+export const YourQuestions = ({ loggedInUser }) => {
   return (
     <div>
+      <h2><u>Your Questions</u></h2>
+      <h3>Username: {loggedInUser}</h3>
       {/* Your Questions content */}
     </div>
   );
