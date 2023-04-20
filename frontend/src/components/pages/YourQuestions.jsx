0eb89db1ff2aab0ae17d1eee2d00541d25533f1a
lawrence-l20 <73035400+lawrence-l20@users.noreply.github.com>
2023-04-19 23:25:29 -0500

    Update YourQuestions.jsx

--- frontend/src/components/pages/YourQuestions.jsx
+++ frontend/src/components/pages/YourQuestions.jsx
@@ -1,29 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { getStudentIDByUsername } from '../../Api';
+import {getQuestionTextByStudentID} from'../../Api';
 
 export const YourQuestions = ({ loggedInUser }) => {
   const [studentID, setStudentID] = useState('');
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState('');
 
   useEffect(() => {
     // Make API call to retrieve student ID for given username
     getStudentIDByUsername(loggedInUser).then(x=>setStudentID(x))
-  }, [loggedInUser]);
+  
+    
+  }, [loggedInUser, studentID]);
 
-  useEffect(() => {
-    // Make API call to retrieve questions for student ID
-    if (studentID) {
-      axios.get(`http://localhost:8000/student/${studentID}/questions`)
-        .then(response => {
-          // If response is successful, set the questions state
-          setQuestions(response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
-  }, [studentID]);
+  
 
   return (
     <div>
@@ -32,9 +23,7 @@ export const YourQuestions = ({ loggedInUser }) => {
       <h3>Student ID: {studentID}</h3>
       <h3>Questions:</h3>
       <ul>
-        {questions.map((question, index) => (
-          <li key={index}>{question.questionText}</li>
-        ))}
+        {questions}
       </ul>
     </div>
   );
