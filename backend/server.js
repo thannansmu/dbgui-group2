e1f3b613eb99db17ea35c1915b3ff527c8bf824d
thannansmu <thannan@smu.edu>
2023-04-23 00:30:14 -0500

    Removed example code from server.js

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,6 @@ const connection = mysql.createConnection({
 
 connection.connect()
 
-// Test API
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
 // Start server
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
