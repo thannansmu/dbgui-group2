f09e20ea4e5123afb90286f3e1e0c51d8b96c099
mhavens17 <112018022+mhavens17@users.noreply.github.com>
2023-04-25 15:17:51 -0500

    setting up booked appointments

--- frontend/src/components/pages/YourAppointments.jsx
+++ frontend/src/components/pages/YourAppointments.jsx
@@ -1,48 +1,53 @@
 import React, { useState, useEffect } from 'react';
 
-export const YourAppointments = () => {
-    
-  
-    return (
-        <div style={{ position: 'relative' }}> 
-        <img 
-          src="/videos/vid_2.gif" 
-          alt="Your GIF" 
-          style={{ 
-            position: 'absolute', 
-            top: 0, 
-            right: 0, 
-            width: '300px', 
-            height: 'auto', 
-            zIndex: 1 
-          }} 
-        />
-        <h1>Your Appointments</h1>
-       <table style={{ borderSpacing: ' 30px' }}>
-         <thead>
-           <tr>
-           <th style={{ width: '30%', fontWeight: 'bold', textDecoration: 'underline' }}>Time</th>
+const AppointmentList = (tutorID, studentID, time, day) => {
+
+};
+
+export const YourAppointments = ({loggedInUser}) => {
+
+  const [bookedAppts, setBookedAppts] = useState([]);
+
+  useEffect(() => {
+    const fetchStudentID = async () => {
+      try {
+        const data = await getStudentIDByUsername(loggedInUser);
+        setStudentID(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchStudentID();
+  }, []);
+
+  return (
+    <div style={{ position: 'relative' }}>
+      <img
+        src="/videos/vid_2.gif"
+        alt="Your GIF"
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          width: '300px',
+          height: 'auto',
+          zIndex: 1
+        }}
+      />
+      <h1>Your Appointments</h1>
+      <table style={{ borderSpacing: ' 30px' }}>
+        <thead>
+          <tr>
+            <th style={{ width: '30%', fontWeight: 'bold', textDecoration: 'underline' }}>Time</th>
             <th style={{ width: '40%', fontWeight: 'bold', textDecoration: 'underline' }}>Tutor</th>
-           </tr>
-         </thead>
-         <tbody>
-           <tr>
-             <td>10:00 AM</td>
-             <td>John Doe</td>
-             <td>Confirmed</td>
-           </tr>
-           <tr>
-             <td>11:00 AM</td>
-             <td>Jane Smith</td>
-             <td>Pending</td>
-           </tr>
-           <tr>
-             <td>1:00 PM</td>
-             <td>Bob Johnson</td>
-             <td>Confirmed</td>
-           </tr>
-         </tbody>
-       </table>
-     </div>
-    );
-  };
+          </tr>
+        </thead>
+        <tbody>
+          {bookedAppts.map((appt) => (
+            <AppointmentList tutorID={appt.tutorID} name={appt.studentID} time={appt.time} day={appt.day} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
